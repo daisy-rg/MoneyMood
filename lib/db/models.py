7b0266aceaa21bcb12d9d5a311af7a6a3ec7171c
daisy <chpdaisy@gmail.com>
2025-05-30 00:48:26 +0300

    did all the models it was nice

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,14 +1,37 @@
-from sqlalchemy import  Column, Integer, String, MetaData, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base
 
-Base= declarative_base(metadata=metadata)
+Base = declarative_base()
 
-class User(base):
-    _tablename_='users'
+class User(Base):
+    __tablename__ = 'users'  
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String(),nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
     age = Column(Integer)
+    profession = Column(String)
 
 
-    
+class Income(Base):
+    __tablename__ = 'income'
+
+    id = Column(Integer, primary_key=True)
+    amount = Column (float , nullable=False)
+    source = =Column (String)
+
+
+
+class Transactions(Base):
+     __tablename__ = 'transactions'
+
+    id = Column(Integer, primary_key=True)
+    amount = Column (float , nullable=False)
+    date = Column(date)
+    category = Column(String nullable=False)
+    description = Column (String())
+
+
+
+
+engine = create_engine('sqlite:///finance.db')
+Base.metadata.create_all(engine)
