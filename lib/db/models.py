c85b522f0315b33eb12439d34868d21a7ff15f6d
daisy <chpdaisy@gmail.com>
2025-05-30 00:09:47 +0300

    User model done

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,4 +1,14 @@
-from sqlalchemy import  Column, Integer, String, MetaData, Table, create_engine
+from sqlalchemy import  Column, Integer, String, MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base= declarative_base(metadata=metadata)
+
+class User(base):
+    _tablename_='users'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(),nullable=False)
+    age = Column(Integer)
+
+
+    
