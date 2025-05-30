b1a81bb7f31a6ea3d6363b266abd8e59478299d2
daisy <chpdaisy@gmail.com>
2025-05-30 14:02:58 +0300

    did the income and transactions

--- lib/db/seed_user.py
+++ lib/db/seed_user.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from faker import Faker
-from models import User, Base
+from datetime import date
+from models import User, Income, Transactions, Base
 
 engine = create_engine('sqlite:///finance.db')
 Session = sessionmaker(bind=engine)
@@ -19,6 +20,24 @@ for i in range (30):
         age=fake.random_int(min=18, max=65),
         profession=fake.job(), 
     )
+
+
+    income = Income(
+        amount=round(fake.random_number(digits=5) / 100, 2),  
+        source=fake.company(),
+        user=user
+    )
+
+
+    transaction = Transactions(
+        amount=round(fake.random_number(digits=4) / 100, 2), 
+        date= fake.date_between(start_date='-1y', end_date='today'),
+        category=fake.word(),
+        description=fake.sentence(),
+        user=user 
+    )
+
+
     users.append(user)
 
 session.add_all(users)
